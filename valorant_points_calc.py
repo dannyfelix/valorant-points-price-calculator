fbe472e67a68df9ae9550766c8b61a49d14984a8
Danny <dannylewis-@live.co.uk>
2021-04-05 03:17:34 +0100

    Adds other region pricing + cleans up code

--- valorant_points_calc.py
+++ valorant_points_calc.py
@@ -1,22 +1,23 @@
-import time
+from math import ceil
+from time import sleep
+import json
 
-# Uses the bundle's VP value as a key to point to its respective price.
-microtx = {
-    525:4.99,
-    1100:9.99,
-    2250:19.99,
-    4000:34.99,
-    5800:49.99,
-    12000:99.99,
-}
+with open('pricing.json') as json_file:
+    data = json.load(json_file)
+
+region = ""
+print("Please type your region")
+while region not in ["AU", "BR", "EU", "IN", "MX", "NZ", "NZ", "NA", "UK"]:
+    region = input("AU, BR, EU, IN, MX, NZ, NA or UK: ")
 
 target = int(input("Enter the amount of Valorant Points (VP) you want to calculate the cost for: "))
+
 print("\nOptionally, you can specify how much VP you currently have to get a more accurate result.")
 curr = int(input("This will deduct it from your specified amount, leaving blank will default to 0: ") or 0)
 
 if curr > target:
     print("You already have enough VP, the program will exit in 5 seconds.")
-    time.sleep(5)
+    sleep(5)
     exit()
 
 target -= curr
@@ -24,21 +25,15 @@ target -= curr
 print("\nTo get", target, "Valorant Points, you could buy...\n")
 
 print('  ----------------------------------------------------------------')
+for key, value in data[f'{region}'].items():
+    diff = target / value[0] # gets the multiplier needed to get from the bundle value to the target 
+    count = ceil(diff) # count of times needed to buy bundle (diff will likely be float so needs to be rounded up)
 
-for x in microtx:
-    calc = 0
-    counter = 0 
-    while calc < target:
-        calc += x
-        counter += 1
-    diff = 0
-    diff = target / x # how much the target is as a percentage compared to the current VP bundle price
     print(
     f'''
-    The [{x}] bundle {counter} time(s) for £{'%0.2f' % (microtx[x]*counter)}, giving you {x*counter} VP,
-    Buying this much would mean {target} VP would cost you £{'%0.2f' % (microtx[x]*diff)}, 
-    This would leave you with {(x*counter)-target} VP left over.
+    The [{value[0]}] bundle {count} time(s) for £{'%0.2f' % (value[1]*count)}, giving you {value[0]*count} VP,
+    Buying this much would mean {target} VP would cost you £{'%0.2f' % (value[1]*diff)}, 
+    This would leave you with {(value[0]*count)-target} VP left over.
     ''')
     print('  ----------------------------------------------------------------')
-    time.sleep(0.5)
 
